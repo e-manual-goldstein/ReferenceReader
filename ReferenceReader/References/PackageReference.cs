e77d544fd7cd6e9ae5c7fae4b05f61bcf12aa3e6
Kevin Shortall <kevinshortall@gmail.com>
2023-07-09 21:45:26 +0100

    Adds PackageReference dependencies

--- ReferenceReader/References/PackageReference.cs
+++ ReferenceReader/References/PackageReference.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Xml.Linq;
 
 namespace ReferenceReader.References
 {
@@ -33,9 +34,39 @@ namespace ReferenceReader.References
 
         public override IEnumerable<ITransitiveDependency> GetTransitiveDependencies(ProjectFile projectFile)
         {
-            // Logic to determine transitive dependencies specific to PackageReference
-            // Return the transitive dependencies as IEnumerable<ITransitiveDependency>
-            yield break;
+            if (!string.IsNullOrEmpty(ActualPath) && Directory.Exists(ActualPath))
+            {
+                string nuspecFilePath = Path.Combine(ActualPath, $"{Name}.nuspec");
+
+                if (File.Exists(nuspecFilePath))
+                {
+                    // Load and parse the .nuspec file
+                    XDocument nuspecDocument = XDocument.Load(nuspecFilePath);
+
+                    // Find the <files> or <files> section within the .nuspec file
+                    XElement filesElement = nuspecDocument.Root?.Element("files") ?? nuspecDocument.Root?.Element("files");
+
+                    if (filesElement != null)
+                    {
+                        foreach (XElement fileElement in filesElement.Elements("file"))
+                        {
+                            string filePath = fileElement.Attribute("src")?.Value;
+                            string fileTarget = fileElement.Attribute("target")?.Value;
+
+                            if (!string.IsNullOrEmpty(filePath) && !string.IsNullOrEmpty(fileTarget) && Path.GetExtension(filePath) == ".dll")
+                            {
+                                string fileName = Path.GetFileName(filePath);
+                                string fileFullPath = Path.Combine(ActualPath, fileTarget, fileName);
+
+                                // Create a DllDependency instance for the dll file
+                                //DllDependency dependency = new DllDependency(fileName, fileFullPath);
+                                
+                                yield return null;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
